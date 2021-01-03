c40facde978ae62d85f2eb5addba25cd4bfc1e1b
Guilherme Andre <guilhermeanm95@hotmail.com>
2021-01-03 01:21:35 -0300

    AddTodo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
 import { useState } from 'react'
 
-function App (){
+const App = () => {
   const [todos, setTodos] = useState(['guilherme', 'felipe'])
+  const [input, setInput] = useState('')
 
+  const addTodo = (e) => {
+    console.log('funcionou')
+    setTodos([...todos, input])
+  }
 
   return (
     <div className="App">
       
 
       <h1>Hello App</h1>
-      <input />
-      <button>Add todo</button>
-
-      <ul>
-      {todos.map(todo => (
-          <li>{todo}</li>))}
-      </ul>
+        <input value={input} onChange={e => setInput(e.target.value)}/>
+          <button onClick={addTodo}>Add todo</button>
+            <ul>
+            {todos.map(todo => (
+                <li>{todo}</li>))}
+            </ul>
     </div>
   );
 }
