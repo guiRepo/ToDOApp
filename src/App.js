7441a7e9437af90ef9b6be4278e1cd8fde18a2d5
Guilherme Andre <guilhermeanm95@hotmail.com>
2021-01-03 15:04:13 -0300

    Todo Component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import './App.css';
-import { useState } from 'react'
+import "./App.css";
+import { useState } from "react";
+import Todo from './components/Todo.js'
+import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
 
 const App = () => {
-  const [todos, setTodos] = useState(['guilherme', 'felipe'])
-  const [input, setInput] = useState('')
+  const [todos, setTodos] = useState(["guilherme", "felipe"]);
+  const [input, setInput] = useState("");
 
   const addTodo = (e) => {
-    setTodos([...todos, input])
-    setInput('')
-    e.preventDefault() // para o refresh causado pelo Submit
-  }
+    setTodos([...todos, input]);
+    setInput("");
+    e.preventDefault(); // para o refresh causado pelo Submit
+  };
 
   return (
-    <div className="App">      
+    <div className="App">
+      <h1>Todo List</h1>
       <form>
-        <h1>Hello App</h1>
-          <input value={input} onChange={e => setInput(e.target.value)}/>
-            <button type="submit" onClick={addTodo}>Add todo</button>
-              <ul>
-              {todos.map(todo => ( // mapeando o state todos para imprimir na li
-                  <li>{todo}</li>))}
-              </ul>
+        <FormControl>
+          <InputLabel>Escreve uma Tarefa</InputLabel>
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+        </FormControl>
+        <Button
+          disabled={!input}
+          type="submit"
+          onClick={addTodo}
+          variant="contained"
+          color="primary"
+        >
+          Add Todo
+        </Button>
       </form>
+      <ul>
+        {todos.map((todo) => ( // mapeando o state todos para imprimir na li
+          <Todo text={todo}/>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
